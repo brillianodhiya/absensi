0b282cdbf6359a5d32d831d2ac235e14ed053f1b
AdStyn <kancell124@gmail.com>
2024-11-07 11:40:26 +0700

    add: halaman login dan menyambungkan API dari backend

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -18,6 +18,12 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
+          href: null,
+        }}
+      />
+      <Tabs.Screen
+        name="home"
+        options={{
           title: "Home",
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon
