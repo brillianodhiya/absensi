141f641d1ff2dc3c95b2d82a38d2c0e2740066b8
AdStyn <kancell124@gmail.com>
2024-11-28 14:09:58 +0700

    add

--- app/(admin)/dashboard.tsx
+++ app/(admin)/dashboard.tsx
@@ -30,7 +30,7 @@ const dashboardUser = () => {
       axios
 
         .get("https://t6c2snf7-3000.asse.devtunnels.ms/users/show_profile", {
- main
+
           headers: {
             Authorization: `Bearer ${token}`,
           },
